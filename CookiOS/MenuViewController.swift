7fb0a5b38e4c7ccbc035d16c379971ffb7cbd2d8
Akira Tsukamoto <akiratsukamoto13@gmail.com>
2019-11-21 12:29:47 -0300

    adiciona suporte ao gamekit

---
+++ CookiOS/MenuViewController.swift
@@ -0,0 +1,71 @@
+import UIKit
+import GameKit
+
+class MenuViewController: UIViewController {
+
+    
+    var hosting = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func join(_ sender: Any) {
+        print("Join pressed")
+        authenticatePlayer()
+    }
+    
+    @IBAction func host(_ sender: Any) {
+        print("Host pressed")
+        hosting = true
+        authenticatePlayer()
+    }
+    
+    func authenticatePlayer() {
+        // Aparecer alguma view de aguarde
+        // Desativar os botões
+        GKLocalPlayer.local.authenticateHandler = handler
+    }
+    
+    func handler(vc: UIViewController?, error: Error?) -> Void {
+        print("Handler return:")
+        if vc != nil
+        {
+            // Ainda não sei direito o que acontece
+            print("VC is not nil!")
+            self.present(vc!, animated: true, completion: nil)
+        }
+        else if GKLocalPlayer.local.isAuthenticated
+        {
+            // Forçar a view ir pra uma VC de menu
+            print("OI")
+            createSession()
+        }
+        else
+        {
+            // Forçar a view ir pra uma VC de unheeee vc nao tem gamecenter chore
+            print("fodeu")
+        }
+    }
+    
+    func createSession() {
+        if hosting {
+            GKMatchmaker.shared().startBrowsingForNearbyPlayers { (player: GKPlayer, reachable) in
+                // Player é o jogador vizinho.
+                // reachable = true -> jogador novo apareceu
+                // reachable = false -> jogador antigo sumiu
+                print(player)
+            }
+        } else {
+            GKMatchmaker.shared().startBrowsingForNearbyPlayers { (player: GKPlayer, reachable) in
+                // Player é o jogador vizinho.
+                // reachable = true -> jogador novo apareceu
+                // reachable = false -> jogador antigo sumiu
+                print(player)
+            }
+        }
+    }
+    
+}
